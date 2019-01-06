91542c31beafa9ee90cd78108ba2731e718777f3
benjaminmilch <benmilch92@gmail.com>
2019-01-06 16:58:32 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/prog2.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ben/CLionProjects/prog2/FileCacheManager.cpp" "/home/ben/CLionProjects/prog2/cmake-build-debug/CMakeFiles/prog2.dir/FileCacheManager.cpp.o"
+  "/home/ben/CLionProjects/prog2/MySerialServer.cpp" "/home/ben/CLionProjects/prog2/cmake-build-debug/CMakeFiles/prog2.dir/MySerialServer.cpp.o"
+  "/home/ben/CLionProjects/prog2/MyTestClientHandler.cpp" "/home/ben/CLionProjects/prog2/cmake-build-debug/CMakeFiles/prog2.dir/MyTestClientHandler.cpp.o"
+  "/home/ben/CLionProjects/prog2/main.cpp" "/home/ben/CLionProjects/prog2/cmake-build-debug/CMakeFiles/prog2.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/prog2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prog2.dir/main.cpp.o"
+  "CMakeFiles/prog2.dir/MySerialServer.cpp.o"
+  "CMakeFiles/prog2.dir/MyTestClientHandler.cpp.o"
+  "CMakeFiles/prog2.dir/FileCacheManager.cpp.o"
+  "prog2.pdb"
+  "prog2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prog2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
